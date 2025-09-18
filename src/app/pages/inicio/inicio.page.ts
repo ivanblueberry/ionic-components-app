dce5ef524fc73c89f0b9091241ffbadcba8d93b6
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-18 12:22:41 -0600

    feat: refactor inicio page to use dynamic component list and improve structure

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,33 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+// Standalones
 import { IonContent,
           IonHeader,
           IonTitle,
           IonToolbar,
-          IonButton,
-          IonRouterLink,
-         } from '@ionic/angular/standalone';
+          IonList,
+          IonItem,
+          IonIcon } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router'
+
+// Icons of Ionic
+import { addIcons } from 'ionicons';
+import { americanFootballOutline,
+         logoAppleAppstore} from 'ionicons/icons';
+
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonButton,
-            IonContent,
-            IonHeader,
-            IonTitle,
-            IonToolbar,
-            CommonModule,
-            FormsModule,
-            IonRouterLink,
-            RouterModule]
+  imports: [IonIcon,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    RouterModule, IonList, IonItem]
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ]
+
+  constructor() {
+    addIcons({americanFootballOutline,
+              logoAppleAppstore
+    });
+   }
 
   ngOnInit() {
   }
