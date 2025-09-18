2e0eac7da9cd6cd4eb6f36f5638dfdd7aede00cd
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-18 10:43:54 -0600

    feat: create alert page with basic structure

---
+++ src/app/pages/alert/alert.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.page.html',
+  styleUrls: ['./alert.page.scss'],
+  standalone: true,
+  imports: [IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class AlertPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
