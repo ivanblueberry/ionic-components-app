919aed0ac9bbe60a62ef516ae97ea11c4b9449eb
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-19 13:43:02 -0600

    feat: refactor action sheet and alert pages to use app-header and implement alert functionality

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonButton, ActionSheetController } from '@ionic/angular/standalone';
+import { HeaderComponent } from "src/app/components/header/header.component";
 
 @Component({
   selector: 'app-action-sheet',
   templateUrl: './action-sheet.page.html',
   styleUrls: ['./action-sheet.page.scss'],
   standalone: true,
-  imports: [IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, CommonModule, FormsModule, HeaderComponent]
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+  constructor( private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
   }
 
+  onClick() {
+    this.presentActionSheet();
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
 }
+
+
