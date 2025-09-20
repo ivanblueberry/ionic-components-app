fef1578276e69efdccbd4983bf1b81b1c162ce13
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-20 11:17:15 -0600

    feat: add date-time page with date selection functionality and basic structure

---
+++ src/app/pages/date-time/date-time.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonList, IonButton, IonListHeader, IonItem, IonDatetime, IonPopover, IonText } from '@ionic/angular/standalone';
+import { HeaderComponent } from "src/app/components/header/header.component";
+import { LargeTitleComponent } from "src/app/components/large-title/large-title.component";
+
+@Component({
+  selector: 'app-date-time',
+  templateUrl: './date-time.page.html',
+  styleUrls: ['./date-time.page.scss'],
+  standalone: true,
+  imports: [IonText, IonPopover, IonDatetime, IonItem, IonListHeader, IonButton, IonList, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, LargeTitleComponent]
+})
+export class DateTimePage implements OnInit {
+
+  fechaNaci: Date = new Date();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  cambioFecha( event: any ) {
+    console.log(event);
+    console.log(new Date( event.detail.value ));
+  }
+
+}
