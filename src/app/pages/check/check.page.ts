858ffd11f517a480886b92d3d3feec73264b467f
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-20 11:16:51 -0600

    feat: add check page with checkbox functionality and basic structure

---
+++ src/app/pages/check/check.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCheckbox } from '@ionic/angular/standalone';
+import { HeaderComponent } from "src/app/components/header/header.component";
+import { LargeTitleComponent } from "src/app/components/large-title/large-title.component";
+
+@Component({
+  selector: 'app-check',
+  templateUrl: './check.page.html',
+  styleUrls: ['./check.page.scss'],
+  standalone: true,
+  imports: [IonCheckbox, IonItem, IonList, IonContent, CommonModule, FormsModule, HeaderComponent, LargeTitleComponent, IonLabel]
+})
+export class CheckPage implements OnInit {
+
+  data = [
+    {
+      name: 'primary',
+      selected: false
+    },
+    {
+      name: 'secondary',
+      selected: true
+    },
+    {
+      name: 'terciary',
+      selected: false
+    },
+    {
+      name: 'success',
+      selected: true
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
