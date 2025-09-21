d0fa6078b4919919abca60518724c1bd9de6a6ef
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-21 11:40:43 -0600

    refactor: replace ion-header with custom app-header in search page; update imports accordingly

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -6,13 +6,14 @@ import { LargeTitleComponent } from "src/app/components/large-title/large-title.
 import { Data } from 'src/app/services/data';
 import { Album } from 'src/app/interfaces/interfaces';
 import { FiltroPipe } from "../../pipes/filtro-pipe";
+import { HeaderComponent } from "src/app/components/header/header.component";
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
   standalone: true,
-  imports: [IonItem, IonList, IonLabel, IonSearchbar, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, LargeTitleComponent, FiltroPipe]
+  imports: [IonItem, IonList, IonLabel, IonSearchbar, IonContent, CommonModule, FormsModule, LargeTitleComponent, FiltroPipe, HeaderComponent]
 })
 export class SearchPage implements OnInit {
 
