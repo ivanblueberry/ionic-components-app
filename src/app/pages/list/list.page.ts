df72e044e1c799e992971c6d49cad43526463e56
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-20 21:25:12 -0600

    feat: enhance user list with sliding item options for favorite, share, and delete actions

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonLabel, IonItem, IonList } from '@ionic/angular/standalone';
+import { IonContent, IonLabel, IonItem, IonList, IonItemSliding, IonItemOptions, IonItemOption, IonIcon } from '@ionic/angular/standalone';
 import { HeaderComponent } from "src/app/components/header/header.component";
 import { LargeTitleComponent } from "src/app/components/large-title/large-title.component";
 import { Data, User } from 'src/app/services/data';
 import { Observable } from 'rxjs';
+
+// Icons of Ionic
+import { addIcons } from 'ionicons';
+import { shareOutline, heartOutline, trashOutline, list } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
   standalone: true,
-  imports: [
+  imports: [IonIcon, IonItemOption, IonItemOptions,
     CommonModule,
     FormsModule,
     IonContent,
@@ -21,16 +26,35 @@ import { Observable } from 'rxjs';
     IonLabel,
     HeaderComponent,
     LargeTitleComponent,
-  ]
+    IonItemSliding
+]
 })
 export class ListPage implements OnInit {
   usuarios!: Observable<any>;
 
-  constructor(private dataService: Data) {}
+  ionList = viewChild<IonList>(IonList);
+
+  constructor(private dataService: Data) {
+    addIcons({heartOutline,shareOutline,trashOutline});
+  }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsuarios(); // Asignar el observable
-    // Opcional: también puedes hacer log
     this.usuarios.subscribe(console.log);
+  }
+
+  favorite( user: any ){
+    console.log('favorite', user);
+    this.ionList()?.closeSlidingItems();
+  }
+
+  share( user: any ) {
+    console.log('share', user);
+    this.ionList()?.closeSlidingItems();
+  }
+
+  trash( user: any ) {
+    console.log('trash', user);
+    this.ionList()?.closeSlidingItems();
   }
 }
