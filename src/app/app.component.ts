5a58d129267eb4c83c085d28d5bff6c957550bce
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-21 16:29:35 -0600

    feat: integrate IonSplitPane for responsive layout in app component; update imports accordingly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { IonApp, IonRouterOutlet, IonContent, IonLabel, IonItem, IonList, IonMenu, IonHeader, IonToolbar, IonTitle, IonCol, IonIcon, MenuController, IonMenuToggle } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonContent, IonLabel, IonItem, IonList, IonMenu, IonHeader, IonToolbar, IonTitle, IonCol, IonIcon, MenuController, IonMenuToggle, IonSplitPane } from '@ionic/angular/standalone';
 import { Componente } from './interfaces/interfaces';
 import { Observable } from 'rxjs';
 import { Data } from './services/data';
@@ -13,7 +13,7 @@ register();
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [CommonModule, RouterLink, IonIcon, IonToolbar, IonList, IonItem, IonContent, IonApp, IonRouterOutlet, IonMenu, IonHeader, IonTitle, IonMenuToggle]
+  imports: [IonSplitPane, CommonModule, RouterLink, IonIcon, IonToolbar, IonList, IonItem, IonContent, IonApp, IonRouterOutlet, IonMenu, IonHeader, IonTitle, IonMenuToggle]
 })
 export class AppComponent implements OnInit {
 
